e8b8b583a9691e1ec66d9cc44cb75b19c097010c
nmarcopo <nmarcopo@nd.edu>
2018-04-28 23:29:53 -0400

    finished thor

--- thor.py
+++ thor.py
@@ -12,11 +12,10 @@ PROCESSES = 1
 REQUESTS  = 1
 VERBOSE   = False
 URL       = None
-ARGUMENTS = sys.argv[1:]
 
 # Functions
 
-def usage(status=0):
+def usage(status):
     print('''Usage: {} [-p PROCESSES -r REQUESTS -v] URL
     -h              Display help message
     -v              Display verbose output
@@ -28,15 +27,31 @@ def usage(status=0):
 
 def do_request(pid):
     ''' Perform REQUESTS HTTP requests and return the average elapsed time. '''
-    
-    return 0
+    TOTALTIME = 0.0
+    for i in range(int(REQUESTS)):
+        try:
+            before = time.time()
+            response = requests.get(URL)
+            after = time.time()
+            data = response.text
+            if VERBOSE:
+                print(data)
+            requestTime = after - before
+            TOTALTIME += requestTime
+            print("Process: {}, Request: {}, Elapsed Time: {:.2f}".format(pid, i, requestTime))
+        except:
+            print("Error in request")
+        
+    print("Process: {}, AVERAGE   , Elapsed Time: {:.2f}".format(pid, TOTALTIME / float(REQUESTS)))
+    return TOTALTIME / float(REQUESTS)
 
 # Main execution
 
 if __name__ == '__main__':
     # Parse command line arguments
+    ARGUMENTS = sys.argv[1:]
     if not ARGUMENTS:
-        usage(0)
+        usage(1)
         
     while len(ARGUMENTS) and len(ARGUMENTS[0]) > 1:
         ARG = ARGUMENTS.pop(0)
@@ -48,10 +63,15 @@ if __name__ == '__main__':
             PROCESSES = int(ARGUMENTS.pop(0))
         elif ARG == '-r':
             REQUESTS = ARGUMENTS.pop(0)
+        elif ARG.startswith('-'):
+            usage(1) # bad arguments
         else:
             URL = ARG
 
     # Create pool of workers and perform requests
     pool = multiprocessing.Pool(PROCESSES)
+    timeIterable = pool.map(do_request, range(PROCESSES))
+
+    print("TOTAL AVERAGE ELAPSED TIME: {:.2f}".format(sum(timeIterable) / PROCESSES))
 
 # vim: set sts=4 sw=4 ts=8 expandtab ft=python:
